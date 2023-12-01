63eee4874acad36444d98cca94564edf67a03d00
ayman_atiia <aymanacio777@gmail.com>
2023-12-02 01:05:46 +0200

    adding route_planner project.

---
+++ CppND-Route-Planning-Project/src/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/aymanacio/cpp_nd/CppND-Route-Planning-Project/thirdparty/googletest/googlemock
+# Build directory: /home/aymanacio/cpp_nd/CppND-Route-Planning-Project/src/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ CppND-Route-Planning-Project/src/thirdparty/pugixml/CMakeFiles/pugixml.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/home/aymanacio/cpp_nd/CppND-Route-Planning-Project/lib/libpugixml.a"
+)
